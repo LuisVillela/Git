5b0027b15199432e81694bb5262a250bb4421b2c
LuisVillela <103969295+LuisVillela@users.noreply.github.com>
2023-05-16 18:43:17 -0600

    LL update

--- geet-activities/geet-activities/geet/utils/init.py
+++ geet-activities/geet-activities/geet/utils/init.py
@@ -52,97 +52,103 @@ def create_branch(path: str) -> object:
             next (Node): pointer to next node in list
         '''
     def __init__(self, hash, message, author, email):
-        self.hash = hash
-        self.message = message
-        self.author = author
-        self.email = email
-        self.next = None
+            self.hash = hash
+            self.message = message
+            self.author = author
+            self.email = email
+            self.next = None
 
 
-class LinkedList:
-    '''
-    Linked List object.
 
-    Attributes:
-        start (Node): pointer to first node in list
+    class LinkedList:
+        '''
+        Linked List object.
 
-    Methods:
-        __init__(self)
-        __iter__(self)
-        traverse(self)
-        insert_first(self, node)
-        insert_last(self, node)
-        remove(key)
-        reverse(self)
-    '''
-    def __init__(self):
-        self.start = None
+        Attributes:
+            start (Node): pointer to first node in list
 
-    def __iter__(self):
-        node = self.start
-        while node:
-            yield node
-            node = node.next
+        Methods:
+            __init__(self)
+            __iter__(self)
+            traverse(self)
+            insert_first(self, node)
+            insert_last(self, node)
+            remove(key)
+            reverse(self)
+        '''
+        def __init__(self):
+            self.start = None
 
-    def traverse(self):
-        if not self.start:
-            print("Linked List is empty.")
-            return
-        node = self.start
-        while node:
-            print(f"Commit Hash: {node.hash}")
-            print(f"Message: {node.message}")
-            print(f"Author: {node.author}")
-            print(f"Email: {node.email}")
-            print()
-            node = node.next
+        def __iter__(self):
+            node = self.start
+            while node:
+                yield node
+                node = node.next
 
-    def insert_first(self, node):
-        if not self.start:
-            self.start = node
-        else:
-            node.next = self.start
-            self.start = node
-
-    def insert_last(self, node):
-        if not self.start:
-            self.start = node
-        else:
-            current = self.start
-            while current.next:
-                current = current.next
-            current.next = node
-
-    def remove(self, key):
-        if not self.start:
-            print("Linked List is empty.")
-            return
-
-        if self.start.hash == key:
-            self.start = self.start.next
-            return
-
-        current = self.start
-        prev = None
-        while current:
-            if current.hash == key:
-                prev.next = current.next
+        def traverse(self):
+            if not self.start:
+                print("Linked List is empty.")
                 return
-            prev = current
-            current = current.next
+            node = self.start
+            while node:
+                print(f"Commit Hash: {node.hash}")
+                print(f"Message: {node.message}")
+                print(f"Author: {node.author}")
+                print(f"Email: {node.email}")
+                print()
+                node = node.next
 
-        print(f"Commit with hash {key} not found.")
+        def insert_first(self, node):
+            if not self.start:
+                self.start = node
+            else:
+                node.next = self.start
+                self.start = node
 
-    def reverse(self):
-        prev = None
-        current = self.start
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        self.start = prev
+        def insert_last(self, node):
+            if not self.start:
+                self.start = node
+            else:
+                current = self.start
+                while current.next:
+                    current = current.next
+                current.next = node
 
-    '''
-    ⬆ Your code ends here.
-    '''
+        def remove(self, key):
+            if not self.start:
+                print("Linked List is empty.")
+                return
+
+            if self.start.hash == key:
+                self.start = self.start.next
+                return
+
+            current = self.start
+            prev = None
+            while current:
+                if current.hash == key:
+                    prev.next = current.next
+                    return
+                prev = current
+                current = current.next
+
+            print(f"Commit with hash {key} not found.")
+        
+            def reverse(self):
+                prev = None
+                current = self.start
+                while current:
+                    next_node = current.next
+                    current.next = prev
+                    prev = current
+                    current = next_node
+                self.start = prev
+
+# Crear una instancia vacía de la lista enlazada y devolverla
+linked_list_instance = LinkedList()
+return linked_list_instance
+
+
+'''
+⬆ Your code ends here.
+'''
